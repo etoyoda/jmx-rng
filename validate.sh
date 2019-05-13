758cb8e58e032ed39eb8ef74592c889517202206
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-05-13 00:08:22 +0000

    VPZU50 yarikake

--- validate.sh
+++ validate.sh
@@ -10,6 +10,11 @@ do
     continue
   fi
   echo "### TRY $xml"
-  jing jmx.rng $xml || break
+  test ! -e validate.log || rm -f validate.log
+  if ! jing jmx.rng $xml > validate.log 2>&1
+  then
+    cat validate.log >&2
+    break
+  fi
   touch "$hush"
 done
